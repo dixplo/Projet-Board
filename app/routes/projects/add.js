086d4eae680ea98f15f6aa434ab5401da4e29e2a
ThomasFont3721 <thomas.fontaine@sts-sio-caen.info>
2020-03-19 11:37:06 +0100

    correction

--- app/routes/projects/add.js
+++ app/routes/projects/add.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { get, set } from '@ember/object';
 
 export default Route.extend({
     actions: {
@@ -8,12 +9,11 @@ export default Route.extend({
             set(model, 'description', '')
             set(model, 'startDate', '')
             set(model, 'endDate', '')
-        }
-        /*
+        },
         save(model) {
-            console.log("save");
-            let proj = this.get('store').createRecord('project', { name: get(model, 'name'), description: get(model, 'description'),startdate: get(model, 'startdate'),duedate: get(model, 'duedate') });
-            proj.save();
+            //console.log("save");
+            //let proj = this.get('store').createRecord('project', { name: get(model, 'name'), description: get(model, 'description'),startdate: get(model, 'startdate'),duedate: get(model, 'duedate') });
+            //proj.save();
             //this.transitionTo('developers')
             //set(model, 'name', '')
             //set(model, 'fname', '')
