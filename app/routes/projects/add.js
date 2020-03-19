93437cd9f6e389ff8a2aea453a3e42c20700099d
theomarie <theomarie.com>
2020-03-19 11:00:48 +0100

    TheoEnfinDansLeProjet

--- app/routes/projects/add.js
+++ app/routes/projects/add.js
@@ -1,4 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    actions: {
+        backToProject(model) {
+            this.transitionTo('projects');
+            set(model, 'name', '')
+            set(model, 'description', '')
+            set(model, 'startdate', '')
+            set(model, 'duedate', '')
+        }
+        /*
+        save(model) {
+            console.log("save");
+            let dev = this.get('store').createRecord('developer', { name: get(model, 'name'), fname: get(model, 'fname') });
+            dev.save();
+            //this.transitionTo('developers')
+            //set(model, 'name', '')
+            //set(model, 'fname', '')
+        }
+        */
+    }
 });
