42544805ff9b8c3053acf2c85d887a5cc5e7bd7a
theomarie <theomarie.com>
2020-03-19 11:29:44 +0100

    saveModel

--- app/routes/projects/add.js
+++ app/routes/projects/add.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { get, set } from '@ember/object';
 
 export default Route.extend({
     actions: {
@@ -9,11 +10,10 @@ export default Route.extend({
             set(model, 'startDate', '')
             set(model, 'endDate', '')
         },
-        
-        save(model) {
-            console.log("save");
-            let proj = this.get('store').createRecord('projects', { name: get(model, 'name'), description: get(model, 'description'),startdate: get(model, 'startdate'),endDate: get(model, 'endDate') });
-            proj.save();
+        save(model) { 
+            //console.log("save");
+            //let proj = this.get('store').createRecord('project', { name: get(model, 'name'), description: get(model, 'description'),startdate: get(model, 'startdate'),duedate: get(model, 'duedate') });
+            //proj.save();
             //this.transitionTo('developers')
             //set(model, 'name', '')
             //set(model, 'fname', '')
