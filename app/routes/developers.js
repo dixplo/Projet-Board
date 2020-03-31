10895c50e9a30cad8eb92681edafa1c8c0b6f921
theomarie <theomarie.com>
2020-03-31 11:23:48 +0200

    cration search bar / menu developer inactive https://github.com/dixplo/Projet-Board/issues/33

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -3,11 +3,27 @@ import { get, set } from '@ember/object';
 import RSVP from 'rsvp';
 
 export default Route.extend({
-    model() {
+    async model() {
+        var content = [];
+        let listDevelopers = await this.store.findAll('developer');
+
+        listDevelopers.forEach(developer => {
+            content.push(
+                {
+                    category: "Developers",
+                    title: developer.username,
+                    description: developer.fullName,
+                    url: "/developer/" + developer.id + "/home"
+                });
+        });
+
+
         return RSVP.hash({
             developers: this.get('store').findAll('developer'),
+            content: content,
             alreadyOpen: ''
         });
+
     },
     actions: {
         openAdd() {
@@ -38,6 +54,25 @@ export default Route.extend({
             }
             set(model, 'alreadyOpen', dev.id)
             this.transitionTo('developers.delete', dev.id);
+        },
+        initUI() {
+            jQuery('.ui.dropdown').dropdown({
+                on: 'hover'
+            });
+            jQuery('.ui.overlay').visibility({ type: 'fixed', offset: 15 });
+    
+            jQuery('#searchBarDev')
+                .search({
+                    source: this.modelFor('developers').content,
+                    searchFields: [
+                        'title', 'description'
+                    ],
+                    type: "category",
+                    fullTextSearch: true,
+                    searchOnFocus: true,
+                    minCharacters: 0,
+                    maxResults: 10
+                });
         }
     }
 });
