726cb790e8bf1971c3789dc8031e86bbd7e7cc31
theomarie <theomarie.com>
2020-04-06 10:53:11 +0200

    profil dev avec listProject Fonctionnel avant refonte design

--- app/routes/developer.js
+++ app/routes/developer.js
@@ -5,9 +5,12 @@ export default Route.extend({
     async model() {
         let devloperId = this.paramsFor("developer").developer_id;
         let developer = await this.get('store').findRecord('developer', devloperId)
+
+        let listProjectDev = await this.get('store').findAll('project', devloperId)
         debugger
         return RSVP.hash({
-            developer: developer
+            developer: developer,
+            listProjectDev: listProjectDev
         });
 
 
