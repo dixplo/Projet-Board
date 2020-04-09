3e03538a9170ae69b0a2a626c39d4ed6ca417820
theomarie <theomarie.com>
2020-04-09 10:47:24 +0200

    edit.js https://github.com/dixplo/Projet-Board/issues/29

--- app/routes/developer/edit.js
+++ app/routes/developer/edit.js
@@ -1,4 +1,23 @@
 import Route from '@ember/routing/route';
+import { get, set } from '@ember/object';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+    async  model() {
+        let currentDevId = localStorage.getItem('developerId')
+        let currentDeveloper = await this.get('store').findRecord('developer', currentDevId)
+        let developer = await this.get('store').findRecord('developer', developerId)
+
+
+        var isMyProfil = false;
+        if (currentDevId == developerId) {
+            isMyProfil = true;
+        }
+
+        return RSVP.hash({
+            developer: developer,
+            connected: JSON.parse(localStorage.getItem('connected')),
+            isMyProfil: isMyProfil
+        });
+    }
 });
