38b4b583f571d30fc38c863e64669d2e8450c681
theomarie <theomarie.com>
2020-04-19 21:21:52 +0200

    Formulaire V1 probleme avec les lanquages https://github.com/dixplo/Projet-Board/issues/29

--- app/routes/developer/edit.js
+++ app/routes/developer/edit.js
@@ -10,6 +10,8 @@ export default Route.extend({
         let developerId = this.paramsFor("developer").developer_id;
         let developer = await this.get('store').findRecord('developer', developerId)
 
+        
+
         if (localStorage.getItem("connected") == "true") {
             if (currentDevId == developerId) {
                 
@@ -22,18 +24,95 @@ export default Route.extend({
             this.transitionTo("developer").developer_id
             return
         }
-
-
-
-
-        
+        debugger
       
 
-
-        
-
         return RSVP.hash({
-
+            developer:developer,
+            selectedImage: "ade",
+            selectedLanguages: developer.languagesString,
+            avatars: ["ade", "bob", "christian", "daniel", "elliot", "elyse", "eve", "ines", "joe", "justen", "laura", "lena", "lindsay", "marie", "mark", "matthew", "molly", "nan", "pablo", "steve", "tom", "veronika"],
+            languages: [".net", "c++", "css", "dart", "html", "java", "javascript", "kotlin", "objective-c", "php", "python", "swift"]
         });
-    }
+       
+
+
+        },
+        actions: {
+        changeImagePlus(model) {
+            let avatars = get(model, "avatars");
+            let index = avatars.indexOf(get(model, "selectedImage"));
+
+            if (avatars.length - 1 > index) {
+                set(model, "selectedImage", avatars[index + 1]);
+            } else {
+                set(model, "selectedImage", avatars[0]);
+            }
+        },
+        changeImageMinus(model) {
+            let avatars = get(model, "avatars");
+            let index = avatars.indexOf(get(model, "selectedImage"));
+
+            if (0 < index) {
+                set(model, "selectedImage", avatars[index - 1]);
+            } else {
+                set(model, "selectedImage", avatars[avatars.length - 1]);
+            }
+        },
+        /* la partie en commentaire me supprime l'affichage des logos des languages 
+        didTransition() {
+            next(this, 'initUI');
+        },
+        */
+        initUI() {
+            $('input').keyup(function () {
+                let model = self.modelFor('register');
+                if (
+                    model.email !== undefined && model.email != "" &&
+                    model.fname !== undefined && model.fname != "" &&
+                    model.name !== undefined && model.name != "" &&
+                    model.username !== undefined && model.username != "" &&
+                    model.selectLanguages == true
+                ) {
+                    set(model, "allFields", true)
+                } else {
+                    set(model, "allFields", false)
+                }
+            });
+            $('.ui.dropdown').dropdown({
+                on: 'hover'
+            });
+            $("#selectLanguagesRegister").change(function () {
+                var selected = jQuery('#selectLanguagesRegister')[0].selectedOptions;
+                let model = self.modelFor('register');
+                if (selected.length > 0) {
+                    set(model, "selectLanguages", true)
+                } else {
+                    set(model, "selectLanguages", false)
+                }
+                set(model, "selectedLanguages", [])
+                for (let option of selected) {
+                    if (!(option instanceof Number)) {
+                        let index = option.index - 1;
+                        if (index != -1) {
+                            get(model, "selectedLanguages").push(get(model, "languages")[index])
+                        }
+                    }
+                }
+                if (
+                    model.email !== undefined && model.email != "" &&
+                    model.fname !== undefined && model.fname != "" &&
+                    model.name !== undefined && model.name != "" &&
+                    model.username !== undefined && model.username != "" &&
+                    model.selectLanguages == true
+                ) {
+                    set(model, "allFields", true)
+                } else {
+                    set(model, "allFields", false)
+                }
+            });
+        }
+
+    
+}
 });
