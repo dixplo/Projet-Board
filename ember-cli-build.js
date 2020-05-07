d63f23f40847920fa563362a9fa77d9547d499a1
ThomasFont3721 <thomas.fontaine@sts-sio-caen.info>
2020-05-07 10:10:53 +0200

    css 3

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    sassOptions: {
-      extension: 'scss'
-    }
+    
   });
 
   // Use `app.import` to add additional libraries to the generated
